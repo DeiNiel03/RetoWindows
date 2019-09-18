a06d80f3ab5a70618dd07ff5c82f9e70ba975b7b
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-09-18 12:24:07 +0200

    Creado menu de seleccion en la clase Launcher

--- src/main/java/RetoWindows/Launcher.java
+++ src/main/java/RetoWindows/Launcher.java
@@ -5,10 +5,38 @@ import java.util.Scanner;
 public class Launcher {
 
 	public static void main(String[] args) {
+		
+		int option;
 		String entrada;
 		Scanner reader = new Scanner(System.in);
-		LectorEntrada lectorEntrada = new LectorEntrada();
+		LectorArchivos lectorArchivos = new LectorArchivos();
 		
-		entrada = lectorEntrada.Lector(reader);
+		// muestra el menu de opciones
+	    System.out.println("SELECCIONA UNA OPCIÓN");
+	    System.out.println("1 - Leer desde entrada estandar");
+	    System.out.println("2 - Leer desde archivo");
+	    System.out.println();
+	    
+	    // recoge la opcion seleccionada
+	    option = reader.nextInt();
+
+	    // Switch construct
+	    switch (option) {
+	    case 1:
+			LectorEntrada lectorEntrada = new LectorEntrada();
+			entrada = lectorEntrada.Lector(reader);
+			System.out.println(entrada);
+	      break;
+	    case 2:
+	    	System.out.println(lectorArchivos.leerArchivoTXT("archivoPrueba.txt"));
+	      break;
+	    default:
+	      System.out.println("Selección invalida");
+	    }
+	    
+	    reader.close();
+		
 	}
+	
+	
 }
--- src/main/java/RetoWindows/LectorArchivos.java
+++ src/main/java/RetoWindows/LectorArchivos.java
@@ -9,18 +9,16 @@ import javax.swing.JOptionPane;
 public class LectorArchivos {
 	
 	/**
-	 * Devuelve el contenido del archivo cuya ruta se le pasa por parametro
+	 * Devuelve el contenido del archivo cuya nombre se le pasa por parametro
 	 * @param ruta
 	 * @return el contenido del archivo en forma de string
 	 */
-	public String leerArchivo(String nombreArchivo) {
+	public String leerArchivoTXT(String nombreArchivo) {
 		String path = System.getProperty("user.dir") + "\\" + nombreArchivo; //BasesLegales.txt
-		FileReader fileReader = null;
 		BufferedReader buffer = null;
 		String resultado = "";
 		try {
-			fileReader = new FileReader(path);
-			buffer = new BufferedReader(fileReader);
+			buffer = new BufferedReader(new FileReader(path));
 			String linea = "";
 			
 			while ((linea = buffer.readLine()) != null) {
